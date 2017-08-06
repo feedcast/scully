076a8c343283bed21f88f9798be36fc0d1d970b4
Marcelo Boeira <me@marceloboeira.com>
2017-08-06 14:36:44 +0200

    feature: Use port from ENV

--- src/main/scala/io/feedcast/scully/Server.scala
+++ src/main/scala/io/feedcast/scully/Server.scala
@@ -3,9 +3,11 @@ package io.feedcast.scully
 import com.twitter.finagle.Http
 import com.twitter.util.Await
 import io.finch._
+import util.Properties
 
 object Server extends App {
-  lazy val root: Endpoint[String] = get(*) { Ok("Hello") }
+  val port = Properties.envOrElse("PORT", "2024").toInt
+  val root = get(*) { Ok("Hello") }
 
-  Await.ready(Http.server.serve(":5000", root.toServiceAs[Text.Plain]))
+  Await.ready(Http.server.serve(s":$port", root.toServiceAs[Text.Plain]))
 }
